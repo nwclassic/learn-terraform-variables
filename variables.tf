c73c4b48f9210abcf187a26a1e0455f9f4673769
Jim Forristal <70030066+nwclassic@users.noreply.github.com>
2021-06-25 15:05:27 -0700

    updated region to use variable

--- variables.tf
+++ variables.tf
@@ -4,3 +4,8 @@ variable "aws_region" {
   type        = string
   default     = "us-west-2"
 }
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
